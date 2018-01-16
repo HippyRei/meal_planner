eef19cacfc4f3805d4694914ae17478bb5160d25
Reilly Mulligan <reillymull@gmail.com>
2018-01-16 01:19:12 -0800

    Added DatabaseHandler.cs with basic constructor and destructor

---
+++ src/ConsoleClient/DatabaseHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.SQLite;
+
+namespace MealPlanner
+{
+    class DatabaseHandler
+    {
+        private const string connectionStringFormat = "Data Source= {0};Version=3;";
+
+        private SQLiteConnection dbConnection;
+        public DatabaseHandler(string dbPath)
+        {
+            dbConnection = new SQLiteConnection(string.Format(connectionStringFormat, dbPath));
+            dbConnection.Open();
+        }
+        
+        ~DatabaseHandler()
+        {
+            dbConnection.Close();
+        }
+    }
+}
