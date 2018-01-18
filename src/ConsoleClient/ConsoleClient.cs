03a840e10596c86c8741c0b8c8c81c9b0bcddfa1
Reilly Mulligan <reillymull@gmail.com>
2018-01-17 23:40:16 -0800

    Removed database from repo and same with data files. Will not be using Git for version control on database.

--- src/ConsoleClient/ConsoleClient.cs
+++ src/ConsoleClient/ConsoleClient.cs
@@ -5,7 +5,7 @@ namespace MealPlanner
 {
 	class ConsoleClient
 	{
-        const string path_to_db = "..\\..\\..\\Data\\MealPlanner.sqlite";
+        const string path_to_db = "C:\\sqlite\\MealPlanner\\MealPlanner.sqlite";
 		static void Main(string[] args)
 		{
             DatabaseHandler h = new DatabaseHandler(path_to_db);
